3c5b3e670931c78e9645a8e59bf5ef9c93d74903
cyberbot501 <softwareengineerjoyc@gmail.com>
2026-04-02 17:19:45 +0100

    almost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -191,8 +191,8 @@ export default function Home() {
         <div className="relative z-10 text-center flex flex-col items-center">
           <p className="text-[#C49B5A] text-sm font-bold tracking-widest uppercase mb-6">Join the Celebration</p>
           <h2 className="text-5xl md:text-7xl font-serif text-white max-w-4xl leading-tight mb-12">
-            Celebrating the legacy <br />
-            <span className="italic">of Travis Uche</span>
+            Celebrating a Lambent<span className="italic font-bold text-7xl md:text-8xl"> Legacy </span><br />
+
           </h2>
           <Link href="https://forms.gle/wQxJ9RKHwYRRfnms9">
             <Button className="bg-[#C49B5A] hover:bg-[#b08b51] text-white rounded-full px-10 py-6 text-lg font-bold border-none cursor-pointer">
